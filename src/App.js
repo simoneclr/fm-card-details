b5f52b4f216b0f04b08573e91b6a6aac0862d13c
simoneclr <simone.clclr@gmail.com>
2022-09-05 15:24:14 +0200

    Extend Tailwind theme with provided style guide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-2xl font-sans">
+    <div className="text-2xl font-grotesk">
       Hello World!
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        gradientStart: "hsl(249, 99%, 64%)",
+        gradientEnd: "hsl(278, 94%, 30%)",
+        errorRed: "hsl(0, 100%, 66%)",
+        lightGrayishViolet: "hsl(270, 3%, 87%)",
+        darkGrayishViolet: "hsl(279, 6%, 55%)",
+        veryDarkViolet: "hsl(278, 68%, 11%)"
+      },
+      fontFamily: {
+        grotesk: ['Space Grotesk', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
