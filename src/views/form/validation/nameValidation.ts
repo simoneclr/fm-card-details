d38f5ddd1845ad97166685950b8e29b6f12d45db
simoneclr <simone.clclr@gmail.com>
2022-09-09 14:49:19 +0200

    Tweak name Validation

--- src/views/form/validation/nameValidation.ts
+++ src/views/form/validation/nameValidation.ts
@@ -2,6 +2,13 @@ import ValidationResult from "./validationResultType";
 
 const validateName: (input: string) => ValidationResult = (input) => {
 
+	if (input.length === 0) {
+		return {
+			error: true,
+			message: "Can't be blank"
+		}
+	}
+
 	let words = input.split(" ")
 
 	if (words.length === 0) {
@@ -20,6 +27,7 @@ const validateName: (input: string) => ValidationResult = (input) => {
 	}
 
 	// TODO: Needs improvement
+	// Consider some names include apostrophees
 	let pattern = /[A-Za-z]+$/
 
 	if (words.filter(w => !pattern.test(w)).length > 0) {
